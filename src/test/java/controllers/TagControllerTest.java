0ad933834c16e3d606b35b7921ac6ee41fdddfbc
Lixuan Mao <mlx@Lixuans-MacBook-Pro.local>
2017-09-03 10:22:49 -0400

    Make test more reasonable.

--- src/test/java/controllers/TagControllerTest.java
+++ src/test/java/controllers/TagControllerTest.java
@@ -12,16 +12,15 @@ public class TagControllerTest {
     @Test
     public void testTagController() {
         TagDao tags = mock(TagDao.class);
-        TagController tagController = mock(TagController.class);
+        TagController tagController = new TagController(tags);
 
-        tags.insert("fat", 2);
-        tags.getAllTaggedReceipts("fat");
-        tags.insert("fat", 2);
+        tagController.toggleTag("fat", 2);
+        tagController.getTaggedReceipts("fat");
+        tagController.toggleTag("fat", 2);
 
         verify(tags).getAllTaggedReceipts("fat");
         verify(tags,times(2)).insert("fat",2);
 
         verifyNoMoreInteractions(tags);
     }
-
 }
